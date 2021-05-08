f2799e579babf29c4c55193ffc5c44ef3e2ad0d3
shubhadeep23 <83590679+shubhadeep23@users.noreply.github.com>
2021-05-08 17:52:52 +0530

    Delete test directory

--- test/src/main/java/com/ecommerce/test/Addressjparepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.test;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface Addressjparepository extends JpaRepository<Address, Integer> {
-
-}
--- test/src/main/java/com/ecommerce/test/Userjparepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.test;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface Userjparepository extends JpaRepository<User, Integer> {
-
-}
